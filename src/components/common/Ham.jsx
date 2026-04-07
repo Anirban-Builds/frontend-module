1c047652a8df3f62838a0c6067861d6846d16efe
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-04-07 11:06:30 +0530

    upd

--- src/components/common/Ham.jsx
+++ src/components/common/Ham.jsx
@@ -3,6 +3,7 @@ import { useUser } from "../../contexts/UserContext"
 import { UseTheme } from "../../contexts/ThemeContext"
 import MOON_PNG from "../../assets/images/moon.png"
 import SUN_PNG from "../../assets/images/sun.png"
+import GITHUB_LOGO from "../../assets/images/github-logo.png"
 import '../../styles/component/ham.css'
 
 const Ham = ({state, setState}) => {
@@ -28,14 +29,20 @@ const Ham = ({state, setState}) => {
 
         <nav className="navbar">
             <ul className={`nav-items ${open ? "open" : ""}`}>
-            <Link to="/" className="home">Home</Link>
-            <Link to="/projects" className="projects">Projects</Link>
+            <li><Link to="/" className="home">Home</Link></li>
+            <li><Link to="/projects" className="projects">Projects</Link></li>
             {user.userExists?
-            <Link to="/account" className={`account ${user.username.length > 10 ? "small-text" : ""}`}>
-                {user.username}</Link>:
-            <Link to="/login" className="login">Login</Link>}
+            <li><Link to="/account" className={`account ${user.username.length > 10 ? "small-text" : ""}`}>
+                {user.username}</Link></li>:
+            <li><Link to="/login" className="login">Login</Link></li>}
             <Link to="/contact" className="contact">Contact</Link>
-             <li className="tog-th-nav">
+
+            <li><Link to="https://github.com/Anirban-Builds"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="git-link"
+            >Git <img src={GITHUB_LOGO}/></Link></li>
+            <li className="tog-th-nav">
                  <button className="theme-btn" onClick={toggleTheme}>
               <span className={theme === "light" ? "icon-moon-light" : "icon-moon-dark"}>
                     <img src={MOON_PNG}/></span>
